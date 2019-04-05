d68aaeb6adfb11999dc4a4f1d21b1e0ebb77f052
NotepadGits <47422859+NotepadGits@users.noreply.github.com>
2019-04-05 15:42:10 +0100

    Lots of art

---
+++ Group project/Assets/Lookat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lookat : MonoBehaviour
+{
+
+    public GameObject self;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        self.transform.Rotate(-65f, -180f, 0f);
+    }
+}
