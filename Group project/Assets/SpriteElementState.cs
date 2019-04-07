e99ca4d920216cd28b3f9b4752aab98899983441
Jacob Lord <47422859+NotepadGits@users.noreply.github.com>
2019-04-08 00:21:04 +0100

    Element buttons added

---
+++ Group project/Assets/SpriteElementState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteElementState : MonoBehaviour
+{
+
+    public Sprite water;
+    public Sprite earth;
+    public Sprite wind;
+    public Sprite fire;
+    public Sprite holy;
+    public Sprite Void;
+
+    private GameObject player;
+
+    private int state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state = player.GetComponent<Attack>().State;
+
+        switch (state)
+        {
+            case 0:
+                GetComponent<SpriteRenderer>().sprite = null;
+                break;
+            case 1:
+                GetComponent<SpriteRenderer>().sprite = water;
+                break;
+            case 2:
+                GetComponent<SpriteRenderer>().sprite = earth;
+                break;
+            case 3:
+                GetComponent<SpriteRenderer>().sprite = wind;
+                break;
+            case 4:
+                GetComponent<SpriteRenderer>().sprite = fire;
+                break;
+            case 5:
+                GetComponent<SpriteRenderer>().sprite = holy;
+                break;
+            case 6:
+                GetComponent<SpriteRenderer>().sprite = Void;
+                break;
+
+        }
+    }
+}
---
+++ Group project/Assets/buttonClickAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonClickAudio : MonoBehaviour
+{
+
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1)) sound.Play();
+        if (Input.GetKeyDown(KeyCode.Keypad2)) sound.Play();
+        if (Input.GetKeyDown(KeyCode.Keypad3)) sound.Play();
+        if (Input.GetKeyDown(KeyCode.Keypad4)) sound.Play();
+        if (Input.GetKeyDown(KeyCode.Keypad5)) sound.Play();
+        if (Input.GetKeyDown(KeyCode.Keypad6)) sound.Play();
+    }
+}
