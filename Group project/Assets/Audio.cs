488cdf63f9e289276b05df15a0a9b0d03d4358b5
Cappuccinni <44321512+Cappuccinni@users.noreply.github.com>
2019-04-09 12:37:50 +0100

    Music

--- Group project/Assets/Audio.cs
+++ Group project/Assets/Audio.cs
@@ -7,19 +7,40 @@ public class Audio : MonoBehaviour
 {
 
     public Sound[] sounds;
-    
+
+    public static Audio instance;
+
     // Start is called before the first frame update
     void Awake()
     {
-        foreach(Sound s in sounds)
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
+
+   void Start()
+    {
+        Play("Bgm");
+    }
+
 
     // Update is called once per frame
     public void Play (string name) 
