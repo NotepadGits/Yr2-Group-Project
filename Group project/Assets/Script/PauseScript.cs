2f9ced188e6c730d7269a46076a35158d7daf372
jackHDwilliams <48690744+jackHDwilliams@users.noreply.github.com>
2019-04-09 13:58:23 +0100

    Me

--- Group project/Assets/Script/PauseScript.cs
+++ Group project/Assets/Script/PauseScript.cs
@@ -12,26 +12,10 @@ public class PauseScript : MonoBehaviour
     public GameObject pausedMenu;
 
 
-    public int timer;
-    public Text countdownText;
-
-    void Start()
-    {
-        StartCoroutine("LoseTime");
-    }
 
 
     void Update()
     {
-        countdownText.text = ("" + timer);
-
-        if(timer <= 0)
-        {
-            StopCoroutine("LoseTime");
-            countdownText.text = "0";
-            Time.timeScale = 0f;
-        }
-
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(GameIsPaused)
@@ -70,15 +54,5 @@ public class PauseScript : MonoBehaviour
     {
         Debug.Log("Exit");
         Application.Quit();
-    }
-
-    IEnumerator LoseTime()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(1);
-
-            timer--;
-        }
     }
 }
